c85542a0854fb3fb2ed481dffcc0716bd8543bf1
Oganesyan Artur <arturris@mail.ru>
2017-10-25 15:29:44 -0500

    Function for Step animation Preparing

--- app/js/main.js
+++ app/js/main.js
@@ -25,11 +25,39 @@ var parallaxInstance = new Parallax(scene);
 /*PRELOADER*/
 setTimeout(function(){
 		$('body').addClass('loaded');
-	}, 3000);
+	}, 600);
 ////////////////////////////////////////
 
 
+/* Scroll Function  */
+ //Firefox
+ $(window).bind('DOMMouseScroll', function(e){
+     if(e.originalEvent.detail > 0) {
+         //scroll down
+         console.log('Down');
+     }else {
+         //scroll up
+         console.log('Up');
+     }
 
+     //prevent page fom scrolling
+     // return false;
+ });
+
+ //IE, Opera, Safari
+ $(window).bind('mousewheel', function(e){
+     if(e.originalEvent.wheelDelta < 0) {
+         //scroll down
+         console.log('Down');
+     }else {
+         //scroll up
+         console.log('Up');
+     }
+
+     //prevent page fom scrolling
+     // return false;
+ });
+////////////////////////////////////
 });
 
 
