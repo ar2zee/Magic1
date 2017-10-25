2a832e6cc631f698068a42e849395dc557a44039
Oganesyan Artur <arturris@mail.ru>
2017-10-25 09:00:17 -0500

    Set Up parallax Js and added preloader to the page. insert -- INSERT __ qw! :qw!

--- app/js/main.js
+++ app/js/main.js
@@ -1,2 +1,35 @@
+$(document).ready(function(){
+
+// Collapse Button for Materialize Nav
+$('.button-collapse').sideNav({
+      menuWidth: 300, // Default is 300
+      closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+      draggable: true, // Choose whether you can drag to open on touch screens,
+      // onOpen: function(el) { /* Do Stuff*/ }, // A function to be called when sideNav is opened
+      // onClose: function(el) { /* Do Stuff*/ }, // A function to be called when sideNav is closed
+    }
+  );
+
+// Resize page to full height of window
+$('.window-height').css('height', $(window).outerHeight() - $('nav').outerHeight()); 
+ $(window).resize(function() {      
+    $('.window-height').css('height', $(window).outerHeight()- $('nav').outerHeight()); 
+  }); 
+ 
+  /* PARALLAX LIBRARY*/
 var scene = document.getElementById('scene');
 var parallaxInstance = new Parallax(scene);
+//////////////////////////////////////////
+
+
+/*PRELOADER*/
+setTimeout(function(){
+		$('body').addClass('loaded');
+	}, 3000);
+////////////////////////////////////////
+
+
+
+});
+
+
