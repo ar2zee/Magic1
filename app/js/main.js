139623c757523eb7f52b1e659890f031b1bf4c42
Oganesyan Artur <arturris@mail.ru>
2017-10-26 16:20:15 -0500

    Set Up Particles JS , Preparing for Section #3

--- app/js/main.js
+++ app/js/main.js
@@ -28,36 +28,11 @@ setTimeout(function(){
 	}, 600);
 ////////////////////////////////////////
 
-
-/* Scroll Function  */
- //Firefox
- $(window).bind('DOMMouseScroll', function(e){
-     if(e.originalEvent.detail > 0) {
-         //scroll down
-         console.log('Down');
-     }else {
-         //scroll up
-         console.log('Up');
-     }
-
-     //prevent page fom scrolling
-     // return false;
- });
-
- //IE, Opera, Safari
- $(window).bind('mousewheel', function(e){
-     if(e.originalEvent.wheelDelta < 0) {
-         //scroll down
-         console.log('Down');
-     }else {
-         //scroll up
-         console.log('Up');
-     }
-
-     //prevent page fom scrolling
-     // return false;
- });
+particlesJS.load('particles-js', 'js/particles.json', function(){
+      
+    });
 ////////////////////////////////////
 });
+
 
 
