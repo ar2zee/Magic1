89ac1ef7737c23e553e27f7526ca02595ace061a
Oganesyan Artur <arturris@mail.ru>
2017-11-08 12:43:40 -0600

    Section#2 Almost Done
    section #2 with almost done animation and layouts

--- app/js/modules/buttonSlider.js
+++ app/js/modules/buttonSlider.js
@@ -14,37 +14,60 @@ $(document).ready(function(){
 		$(btnLeft).on('click', function(e){
 			$(handle).animate({'left': 2374}, 1500);
 			$(topLayer).animate({'width': 3375}, 1500, function(){
-				console.log('Hey ITS LEFT');
+				$('#closeBtn1, #closeBtn2').css({'display': 'block'});
 				$('.bottom, .top').css('display', 'none');
+				$('#closeBtn1, #closeBtn2, .btn_position').animate({'opacity': 1}, 1000, function(){
+					$('#closeME').css({'display': 'none'});
+					$('#what-we-do').css({'display': 'block', 'opacity': 1});
+
+				});
 			});
-			$('#closeBtn').css({'display': 'block'});
-			$('#closeBtn').animate({'opacity': 1}, 1000);
+
 			return false;
 		});
 
 		$(btnRight).on('click', function(e){
 			$(handle).animate({'left': -334}, 1500);
 			$(topLayer).animate({'width': 666.5}, 1500, function(){
-				console.log('Hey ITS RIGHT');
+				$('#closeBtn1, #closeBtn2').css({'display': 'block'});
 				$('.bottom, .top').css('display', 'none');
+				$('#closeBtn1, #closeBtn2, .btn_position').animate({'opacity': 1}, 1000, function(){
+					$('#closeME').css({'display': 'none'});
+					$('#who-we-are').css({'display': 'block', 'opacity': 1});
+
+				});
 			});
-			$('#closeBtn').css({'display': 'block'});
-			$('#closeBtn').animate({'opacity': 1}, 1000);
+
 			return false;
 		});
 
-			$('#closeBtn').on('click', function(e){
+			
+
+			$('#closeBtn1, #closeBtn2').on('click', function(e){
 				$(handle).animate({'left': 51+'vw'}, 1500, function(){
-				console.log('Hey ITS CLOSING');
+				$('#closeBtn1, #closeBtn2').css({'opacity': 0});
+				$('#closeBtn1, #closeBtn2').css({'display': 'none'});
 				$('.bottom, .top').css('display', 'block');
+				$('#what-we-do').css({'display': 'none', 'opacity': 0});
+				$('#who-we-are').css({'display': 'none', 'opacity': 0});
+				$('.bottom, .top').animate('opacity', 1);
+				$('#closeME').css({'display': 'block'});
+
+				console.log('bitton pressed inside');
 			});
 				$(topLayer).animate({'width': 100+'vw'}, 1500);
-				$(this).css({'opacity': 0});
-				$(this).css({'display': 'none'});
+				
+				console.log('bitton pressed outside');
+				
 				return false;
 			});
 			
+  $('.carousel').carousel();
 
+             // Init Carousel Slider
+            $('.carousel.carousel-slider').carousel({fullwidth:false, indicators:true});
+
+ $('.slider').slider();
 	
 });
 
