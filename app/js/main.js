03d57fddd962c99512d19ad08173b11b57861b42
Oganesyan Artur <arturris@mail.ru>
2017-11-15 09:57:35 -0600

    Basic layout finished (map, 1st part of contact ,image size, content)

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
 
+
 // Collapse Button for Materialize Nav
 $('.button-collapse').sideNav({
       menuWidth: 300, // Default is 300
@@ -12,6 +13,27 @@ $('.button-collapse').sideNav({
 
        $('.modal').modal();
        $('.parallax').parallax();
+
+
+    var options = [
+      {selector: '.section1', offset: ($('.section1').offset().top)/2, callback: function(el) {
+        ($('.section1').animate({'opacity': 1})),600 ;
+        
+      } },
+      {selector: '.section2', offset: ($('.section2').offset().top)/2, callback: function(el) {
+      ($('.section2').animate({'opacity': 1})),600 ;
+        
+      } },
+      {selector: '.section3', offset: ($('.section3').offset().top)/4, callback: function(el) {
+        ($('.section3').animate({'opacity': 1})),600 ;
+      } },
+     
+      {selector: '.section4', offset: ($('.section4').offset().top)/4, callback: function(el) {
+        ($('.section4').animate({'opacity': 1})),600 ;
+      }}
+
+    ];
+    Materialize.scrollFire(options);
 
 // Resize page to full height of window
 // $('.window-height').css('height', $(window).outerHeight()); 
@@ -33,9 +55,152 @@ setTimeout(function(){
 particlesJS.load('particles-js', 'js/modules/particles.json', function(){});
 ////////////////////////////////////
  
-
-
 });
 
+function initMap() {
+        // Create a map object and specify the DOM element for display.
+          
+        var map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 29.817402, lng: -95.5413744},
+           zoom: 3,
+          styles: [
+            {elementType: 'geometry', stylers: [{color: '#ebe3cd'}]},
+          {elementType: 'labels.text.fill', stylers: [{color: '#523735'}]},
+          {elementType: 'labels.text.stroke', stylers: [{color: '#f5f1e6'}]},
+          {
+            featureType: 'administrative',
+            elementType: 'geometry.stroke',
+            stylers: [{color: '#c9b2a6'}]
+          },
+          {
+            featureType: 'administrative.land_parcel',
+            elementType: 'geometry.stroke',
+            stylers: [{color: '#dcd2be'}]
+          },
+          {
+            featureType: 'administrative.land_parcel',
+            elementType: 'labels.text.fill',
+            stylers: [{color: '#ae9e90'}]
+          },
+          {
+            featureType: 'landscape.natural',
+            elementType: 'geometry',
+            stylers: [{color: '#dfd2ae'}]
+          },
+          {
+            featureType: 'poi',
+            elementType: 'geometry',
+            stylers: [{color: '#dfd2ae'}]
+          },
+          {
+            featureType: 'poi',
+            elementType: 'labels.text.fill',
+            stylers: [{color: '#93817c'}]
+          },
+          {
+            featureType: 'poi.park',
+            elementType: 'geometry.fill',
+            stylers: [{color: '#a5b076'}]
+          },
+          {
+            featureType: 'poi.park',
+            elementType: 'labels.text.fill',
+            stylers: [{color: '#447530'}]
+          },
+          {
+            featureType: 'road',
+            elementType: 'geometry',
+            stylers: [{color: '#fff1e6'}]
+          },
+          {
+            featureType: 'road.arterial',
+            elementType: 'geometry',
+            stylers: [{color: '#fdfcf8'}]
+          },
+          {
+            featureType: 'road.highway',
+            elementType: 'geometry',
+            stylers: [{color: '#f8c967'}]
+          },
+          {
+            featureType: 'road.highway',
+            elementType: 'geometry.stroke',
+            stylers: [{color: '#e9bc62'}]
+          },
+          {
+            featureType: 'road.highway.controlled_access',
+            elementType: 'geometry',
+            stylers: [{color: '#e98d58'}]
+          },
+          {
+            featureType: 'road.highway.controlled_access',
+            elementType: 'geometry.stroke',
+            stylers: [{color: '#db8555'}]
+          },
+          {
+            featureType: 'road.local',
+            elementType: 'labels.text.fill',
+            stylers: [{color: '#806b63'}]
+          },
+          {
+            featureType: 'transit.line',
+            elementType: 'geometry',
+            stylers: [{color: '#dfd2ae'}]
+          },
+          {
+            featureType: 'transit.line',
+            elementType: 'labels.text.fill',
+            stylers: [{color: '#8f7d77'}]
+          },
+          {
+            featureType: 'transit.line',
+            elementType: 'labels.text.stroke',
+            stylers: [{color: '#ebe3cd'}]
+          },
+          {
+            featureType: 'transit.station',
+            elementType: 'geometry',
+            stylers: [{color: '#dfd2ae'}]
+          },
+          {
+            featureType: 'water',
+            elementType: 'geometry.fill',
+            stylers: [{color: '#b9d3c2'}]
+          },
+          {
+            featureType: 'water',
+            elementType: 'labels.text.fill',
+            stylers: [{color: '#92998d'}]
+          }
+          ]
+        });
 
+        var layer = new google.maps.FusionTablesLayer({
+    query: {
+            select: 'geometry',
+            from: '1ertEwm-1bMBhpEwHhtNYT47HQ9k2ki_6sRa-UQ'
+          },
+          styles: [{
+            polygonOptions: {
+              fillColor: '#00FF00',
+              fillOpacity: 0.3
+            }
+          }, {
+            where: 'birds > 300',
+            polygonOptions: {
+              fillColor: '#0000FF'
+            }
+          }, {
+            where: 'population > 5',
+            polygonOptions: {
+              fillOpacity: 1.0
+            }
+          }]
+    
+  });
 
+  layer.setMap(map);
+
+       
+       
+};
--- app/js/modules/buttonSlider.js
+++ app/js/modules/buttonSlider.js
@@ -1,14 +1,26 @@
 $(document).ready(function(){
 	let wrapper = document.getElementById('wrapper');
+
+	let wrapper2 = document.getElementById('wrapper2');
+
 	let topLayer = wrapper.querySelector('.top');
 	let handle = wrapper.querySelector('.handle');
+
+	let topLayer2 = wrapper2.querySelector('.top');
+	let handle2 = wrapper2.querySelector('.handle');
+
 	let skew = 0;
 	let delta = 0;
+
 	let btnLeft = document.getElementById('button_left');
 	let btnRight = document.getElementById('button_right');
+
+	let btnLeft2 = document.getElementById('button_left2');
+	let btnRight2 = document.getElementById('button_right2');
+
 	let width = $('.skewed .top').width();
 			
-			if(wrapper.className.indexOf('skewed') != -1 ){
+			if(wrapper.className.indexOf('skewed') != -1 && wrapper2.className.indexOf('skewed') != -1){
 						skew = 1000;
 				}	
 
@@ -42,7 +54,38 @@ $(document).ready(function(){
 				$(this).css({'display': 'none'});
 				return false;
 			});
-			
+				//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+				$(btnLeft2).on('click', function(e){
+			$(handle2).animate({'left': 2374}, 1500);
+			$(topLayer2).animate({'width': 3375}, 1500, function(){
+					
+			});
+			$('#closeBtn2').css({'display': 'block'});
+			$('#closeBtn2').animate({'opacity': 1}, 1000);
+			return false;
+		});
+
+		$(btnRight2).on('click', function(e){
+			$(handle2).animate({'left': -334}, 1500);
+			$(topLayer2).animate({'width': 666.5}, 1500, function(){
+
+			});
+			$('#closeBtn2').css({'display': 'block'});
+			$('#closeBtn2').animate({'opacity': 1}, 1000);
+			return false;
+		});
+
+			$('#closeBtn2').on('click', function(e){
+				$(handle2).animate({'left': '50vw', 'top': '50%'}, 1500, function(){
+				$('.bottom, .top').css('display', 'block');
+			});
+				// $(topLayer).animate({'width': 100+'vw'}, 1500);
+				  $('.skewed .top').animate({'width': width}, 1500);
+				$(this).css({'opacity': 0});
+				$(this).css({'display': 'none'});
+				return false;
+			});
 
 
 	
